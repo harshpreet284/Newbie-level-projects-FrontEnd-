239a6449def1f818eee43b96254a75c5b3e2582f
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-02-16 15:49:58 +0530

    add

---
+++ Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+          <div>P E R F U M E</div>
+          <div>Gabrielle Essence Eau De Parfum</div>
+          <div>A floral,solar and voluptous interpretation composed by Olivier Polge,Perfumer-Creator for the House of CHANEL.</div>
+           <div>
+            <div>$149.99</div>
+            <div>$169.99</div>
+           </div>
+           <div>
+            <button>Add to Cart</button>
+           </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard

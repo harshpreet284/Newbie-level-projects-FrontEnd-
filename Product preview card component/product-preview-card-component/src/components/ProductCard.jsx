61f95e8c0063e79e6dff11fc6681d9ac0d20015e
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-02-18 15:47:02 +0530

    add

--- Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
+++ Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
@@ -4,23 +4,27 @@ const ProductCard = () => {
   return (
     <div>
     <div className='bg-[#F3EBE3] w-screen h-screen flex items-center justify-center'>
-      <div className='bg-white w-xl h-1/2 '>
+      <div className='bg-white w-xl h-auto flex rounded-md'>
         <div>
-           <img className='w-24' src="./src/assets/images/image-product-desktop.jpg" alt="" />
+           <img className='w-auto h-auto' src="./src/assets/images/image-product-desktop.jpg" alt="" />
         </div>
-        <div >
-          <div>P E R F U M E</div>
-          <div>Gabrielle Essence Eau De Parfum</div>
-          <div>A floral,solar and voluptous interpretation composed by Olivier Polge,Perfumer-Creator for the House of CHANEL.</div>
-           <div>
-            <div>$149.99</div>
-            <div>$169.99</div>
+         <div >
+        <div>
+          <div><p className='pt-3 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>P E R F U M E</p></div>
+          <div><p className='pt-3 pl-5 font-[Montserrat] font-extrabold text-2xl'>Gabrielle Essence Eau De Parfum</p></div>
+          <div><p className='pt-3 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>A floral,solar and voluptous interpretation composed by Olivier Polge,Perfumer-Creator for the House of CHANEL.</p></div>
+           <div className='flex'>
+            <div><p className='pt-3 pl-5 font-[Fraunces] text-[#3c8067] font-extrabold text-3xl'>$149.99</p></div>
+            <div className='pt-5 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'><s>$169.99</s></div>
            </div>
-           <div>
-            <button>Add to Cart</button>
+           <div className='flex items-center justify-center bg-[#3c8067] m-8 w-56 h-10 rounded-md'>
+            <div><img src="./src/assets/images/icon-cart.svg" alt="" /></div>
+            <div><button className=' text-white font-[Fraunces] font-extrabold text-[14px] ml-4'>Add to Cart</button></div>
            </div>
         </div>
       </div>
+      </div>
+     
     </div>
     </div>
   )

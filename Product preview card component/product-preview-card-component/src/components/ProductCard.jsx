65e0096431eb09f7426b7a5305de728026f4dc2a
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-02-19 17:25:53 +0530

    add

--- Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
+++ Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
@@ -5,21 +5,33 @@ const ProductCard = () => {
     <div>
     <div className='bg-[#F3EBE3] w-screen h-screen flex items-center justify-center'>
       <div className='bg-white w-xl h-auto flex rounded-md'>
-        <div>
-           <img className='w-auto h-auto' src="./src/assets/images/image-product-desktop.jpg" alt="" />
+        <div className='w-960'>
+           <img loading='lazy' className='w-full h-full rounded-l-md' src="./src/assets/images/image-product-desktop.jpg" alt="" />
         </div>
          <div >
         <div>
-          <div><p className='pt-3 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>P E R F U M E</p></div>
-          <div><p className='pt-3 pl-5 font-[Montserrat] font-extrabold text-2xl'>Gabrielle Essence Eau De Parfum</p></div>
-          <div><p className='pt-3 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>A floral,solar and voluptous interpretation composed by Olivier Polge,Perfumer-Creator for the House of CHANEL.</p></div>
+          <div><p className='pt-6 pl-7 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>P E R F U M E</p></div>
+          <div>
+            <pre className='pt-4 pl-7 font-[Montserrat] font-extrabold text-3xl text-[#1c232b]'>{`Gabrielle 
+Essence Eau 
+De Parfum`}
+</pre>
+            </div>
+          <div>
+            <pre className='pt-6 pl-7 text-[17px] font-[Fraunces] font-extralight text-[#6E6E87]'>{`A floral,solar and voluptous 
+interpretation composed by
+Olivier Polge,Perfumer-Creator
+for the House of CHANEL.`}
+</pre>
+            </div>
            <div className='flex'>
-            <div><p className='pt-3 pl-5 font-[Fraunces] text-[#3c8067] font-extrabold text-3xl'>$149.99</p></div>
-            <div className='pt-5 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'><s>$169.99</s></div>
+            <div><p className='pt-6 pl-8 font-[Fraunces] text-[#3c8067] font-extrabold text-3xl'>$149.99</p></div>
+            <div className='pt-8 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'><s>$169.99</s></div>
            </div>
-           <div className='flex items-center justify-center bg-[#3c8067] m-8 w-56 h-10 rounded-md'>
+           
+            <div className='flex items-center justify-center bg-[#3c8067] m-8 w-56 h-10 rounded-md hover:bg-[#1b4133]'>
             <div><img src="./src/assets/images/icon-cart.svg" alt="" /></div>
-            <div><button className=' text-white font-[Fraunces] font-extrabold text-[14px] ml-4'>Add to Cart</button></div>
+            <div><a href="#"><button className=' text-white font-[Fraunces] font-extrabold text-[14px] ml-4'>Add to Cart</button></a></div>
            </div>
         </div>
       </div>

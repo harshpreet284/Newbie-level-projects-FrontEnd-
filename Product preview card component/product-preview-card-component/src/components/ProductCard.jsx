3ea7819ec4703fc1b520cf56440f952f4d853bbd
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-02-22 21:35:17 +0530

    add

--- Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
+++ Product preview card component/product-preview-card-component/src/components/ProductCard.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 const ProductCard = () => {
   return (
     <div className='m-0 p-0 box-border'>
-    <div className='bg-[#F3EBE3]  w-screen h-screen flex items-center justify-center'>
-      <div className='bg-white w-auto h-auto sm:w-xl sm:h-auto sm:flex  rounded-md'>
+    <div className='bg-[#F3EBE3]  w-screen h-screen flex items-center justify-center overflow-auto'>
+      <div className='bg-white w-auto h-auto sm:w-xl sm:h-auto sm:flex  rounded-md '>
         <div className='w-72 sm:w-960'>
-           <img loading='lazy' className='w-full h-full rounded-l-md ' src="./src/assets/images/image-product-desktop.jpg" alt="" />
+          <picture>
+            <source srcSet='./src/assets/images/image-product-desktop.jpg' media='(min-width: 640px)'/>
+            <img loading='lazy' className='w-full h-full rounded-l-md 'src="./src/assets/images/image-product-mobile.jpg" alt="" />
+          </picture>
         </div>
          <div >
         <div>
-          <div><p className='pt-6 pl-7 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>P E R F U M E</p></div>
+          <div><p className='pt-4 pl-7 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'>P E R F U M E</p></div>
           <div>
             <pre className='pt-4 pl-7 font-[Montserrat] font-extrabold text-3xl text-[#1c232b]'>{`Gabrielle 
 Essence Eau 
@@ -18,15 +21,15 @@ De Parfum`}
 </pre>
             </div>
           <div>
-            <pre className='pt-6 pl-7 text-[17px] font-[Fraunces] font-extralight text-[#6E6E87]'>{`A floral,solar and voluptous 
+            <pre className='pt-4 pl-7 text-[17px] font-[Fraunces] font-extralight text-[#6E6E87]'>{`A floral,solar and voluptous 
 interpretation composed by
 Olivier Polge,Perfumer-Creator
 for the House of CHANEL.`}
 </pre>
             </div>
            <div className='flex'>
-            <div><p className='pt-6 pl-8 font-[Fraunces] text-[#3c8067] font-extrabold text-3xl'>$149.99</p></div>
-            <div className='pt-8 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'><s>$169.99</s></div>
+            <div><p className='pt-4 pl-8 font-[Fraunces] text-[#3c8067] font-extrabold text-3xl'>$149.99</p></div>
+            <div className='pt-4 pl-5 text-[14px] font-[Fraunces] font-extralight text-[#6E6E87]'><s>$169.99</s></div>
            </div>
            
             <div className='flex items-center justify-center bg-[#3c8067] m-8 w-56 h-10 rounded-md hover:bg-[#1b4133]'>

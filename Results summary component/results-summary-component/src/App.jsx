1e4771e5d940885df2711c61be70b55c19fe676c
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-02-23 20:53:37 +0530

    add

---
+++ Results summary component/results-summary-component/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ResultSummary from './components/ResultSummary'
+
+function App() {
+  
+
+  return (
+    <>
+     <ResultSummary></ResultSummary>
+    </>
+  )
+}
+
+export default App
---
+++ Results summary component/results-summary-component/src/components/ResultSummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ResultSummary = () => {
+  return (
+    <div className='m-0 p-0 box-border'>
+     <div className='bg-[#ebf1ff] h-screen w-screen flex items-center justify-center'>
+      <div className='bg-white h-auto w-auto '>
+        Hello
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default ResultSummary
